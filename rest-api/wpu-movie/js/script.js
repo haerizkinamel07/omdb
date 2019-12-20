1b98f5ff82b6af611e816ba13e802499151edf0d
haerizkinamel07 <ahaerizkina@gmail.com>
2019-12-20 13:12:53 +0700

    menampilkan data pencarian

--- rest-api/wpu-movie/js/script.js
+++ rest-api/wpu-movie/js/script.js
@@ -8,7 +8,28 @@ $('#search-button').on('click', function(){
         's': $('#search-input').val()
     },
     success: function (result) {
-        console.log(result);
+        if(result.Response == "True"){
+            let movies = result.search;
+            $each(movies, function (i,data){
+                $('#movie-list').append(`
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src=""` + data.poster +`class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">`+ data.Title +`</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6
+                            <a href="#" class="card-link">See Detail</a>
+                    </div>
+                    </div>
+                `)
+            });
+        }else{
+            $('#movie-list').html(`
+            <div class ="col">
+            <h1 class="text-center">`+ result.Error +`</h1>
+            </div>
+            `)
+        }
     }
     });
 });

4cbb721f28b0ee4aede96cb2574d5e9ce11bde22
haerizkinamel07 <ahaerizkina@gmail.com>
2019-12-20 14:53:22 +0700

    menambahkan fungsi modal

--- rest-api/wpu-movie/js/script.js
+++ rest-api/wpu-movie/js/script.js
@@ -20,7 +20,7 @@ function searchMovie(){
                         <div class="card-body">
                             <h5 class="card-title">`+ data.Title +`</h5>
                             <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
-                            <a href="#" class="card-link">See Detail</a>
+                            <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+ data.imdbID +`">See Detail</a>
                     </div>
                     </div>
                     </div>
@@ -48,4 +48,39 @@ $('#search-input').on('keyup', function(e){
     if(e.which === 13){
         searchMovie();
     }
-})
+});
+
+$('#movie-list').on('click','.see-detail', function(){
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey' : 'c94ffec',
+            'i': $(this).data('id')
+        },
+        success: function(movie){
+            if( movie.Response === "True"){
+                $('.modal-body').html(`
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="`+ movie.Poster + `"class="img-fluid">
+                        </div>
+
+                        <div class="col-md-8"> 
+                        <ul class="list-group">
+                            <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                            <li class="list-group-item">Realesed :`+ movie.Released +`</li>
+                            <li class="list-group-item">Genre :`+ movie.Genre +`</li>
+                            <li class="list-group-item">Director :`+ movie.Director +`</li>
+                            <li class="list-group-item">Actor :`+ movie.Actor +`</li>
+                         </ul>
+                        </div>
+                        </div>
+                        </div>
+                `)
+            }
+          }
+        });
+    });
